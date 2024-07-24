9de104d4810b907290fbf991315a5fda669329f0
maram.khalfaoui <maram.khalfaoui@etudiant-enit.utm.tn>
2024-07-24 13:01:53 +0100

    Implement CsvService and ArtistService for The Artist Search App'

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const API_KEY = 'ef08df013439569acbf8f9a4b7ec6db8';
+export const API_KEY = process.env.APIkey;
 export const BASE_URL = `http://ws.audioscrobbler.com/2.0/?method=artist.search`;
 export const PORT = process.env.PORT || 3000;
 export const csvDirectory = process.env.CSV_DIRECTORY || './data/csv_files';
