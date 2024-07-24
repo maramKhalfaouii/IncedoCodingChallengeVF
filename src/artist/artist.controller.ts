78f2853a87061410c7bead78e6811eca616042f1
maram.khalfaoui <maram.khalfaoui@etudiant-enit.utm.tn>
2024-07-24 15:26:11 +0100

    Improve the swagger docs

--- src/artist/artist.controller.ts
+++ src/artist/artist.controller.ts
@@ -7,12 +7,23 @@ import {
 } from '@nestjs/common';
 import { ArtistService } from './artist.service';
 import { SearchArtistDto } from '../artist/dto/search-artist.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ArtistResponseDto } from './dto/artist-response.dto';
 
 @Controller('artists')
 export class ArtistController {
   constructor(private readonly artistService: ArtistService) {}
 
   @Get('search')
+  @ApiOperation({ summary: 'Search and save artists to CSV' })
+  @ApiResponse({
+    status: 200,
+    description: 'CSV file created successfully.',
+    type: ArtistResponseDto,
+    isArray: true,
+  })
+  @ApiResponse({ status: 404, description: 'No artists found.' })
+  @ApiResponse({ status: 500, description: 'Failed to create CSV file.' })
   @UsePipes(new ValidationPipe())
   async searchArtist(@Query() searchArtistDto: SearchArtistDto) {
     const { name, filename } = searchArtistDto;
